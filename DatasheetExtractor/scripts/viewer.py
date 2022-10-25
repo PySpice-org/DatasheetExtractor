876df6bfae777c94bc04b41bb38e8f7c434e9716
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2022-10-25 19:32:14 +0200

    add viewer script

---
+++ DatasheetExtractor/scripts/viewer.py
@@ -0,0 +1,39 @@
+####################################################################################################
+#
+# DatasheetExtractor - A Python library to extract data from datasheet
+# Copyright (C) 2022 Fabrice Salvaire
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+####################################################################################################
+
+from DatasheetExtractor import setup_logging
+setup_logging()
+
+import logging
+
+import os
+os.environ['PYSIDE63_OPTION_PYTHON_ENUM'] = '1'
+os.environ['QT_API'] = 'pyside6'
+
+from DatasheetExtractor.frontend.QmlApplication import Application
+
+####################################################################################################
+
+def main() -> None:
+    logging.info('Start ...')
+    # application = Application()
+    Application.setup_gui_application()
+    application = Application.create()
+    application.exec_()
