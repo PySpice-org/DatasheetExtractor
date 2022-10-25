64a8c863b795714d71aa4dbf52b8338889856cdf
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2022-10-25 19:33:15 +0200

    setenv

--- setenv.sh
+++ setenv.sh
@@ -3,3 +3,8 @@ export DatasheetExtractor_examples_path=${DatasheetExtractor_source_path}/exampl
 
 source /opt/python-virtual-env/py310/bin/activate
 append_to_python_path_if_not ${DatasheetExtractor_source_path}
+append_to_python_path_if_not ${DatasheetExtractor_source_path}/mamba-dist/
+append_to_ld_library_path_if_not ${DatasheetExtractor_source_path}/mamba-dist/
+
+# Fixme: libQt6PdfQuick.so.6
+append_to_ld_library_path_if_not /home/opt/python-virtual-env/py310/lib/python3.10/site-packages/PyQt6/Qt6/lib/
