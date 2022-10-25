964ef7affd80d41881d591fd9c1a2c6fb35704ff
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2022-10-25 23:25:49 +0200

    icon task

--- tasks/icon.py
+++ tasks/icon.py
@@ -21,6 +21,7 @@
 ####################################################################################################
 
 from pathlib import Path
+import os
 
 from invoke import task
 
@@ -34,11 +35,13 @@ ICONS_PATH = SOURCE_PATH.joinpath('DatasheetExtractor', 'frontend', 'rcc', 'icon
 ####################################################################################################
 
 @task
-def fetch_icon(ctx, src_name, dst_name, style='baseline', color='black', version=12):
+def fetch_icon(ctx, src_name, dst_name=None, style='baseline', color='black', version=12):
      # style: [baseline], outline, round, twotone, sharp
      # color: [black], white
     theme = 'material'
     print('Icons path:', ICONS_PATH, theme)
+    if dst_name is None:
+        dst_name = src_name.replace('_', '-')
     print(f'{src_name} -> {dst_name}   style={style} color={color} version={version}')
     fetcher = MaterialIconFetcher(ICONS_PATH, theme)
     fetcher.fetch_icon(
@@ -48,3 +51,18 @@ def fetch_icon(ctx, src_name, dst_name, style='baseline', color='black', version
         color,
         version
     )
+
+####################################################################################################
+
+@task
+def fix_name(ctx):
+    for directory, _, filenames in os.walk(ICONS_PATH):
+        directory = Path(directory)
+        for filename in filenames:
+            path = directory.joinpath(filename)
+            if path.suffix == '.png':
+                if '_' in filename:
+                    new_filename = filename.replace('_', '-')
+                    new_path = directory.joinpath(new_filename)
+                    print(path, new_path)
+                    path.rename(new_path)
