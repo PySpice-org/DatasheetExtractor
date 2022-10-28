316aa931f45292b06717f2efd22471f5d34fdcfc
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2022-10-28 22:25:58 +0200

    setenv

--- setenv.sh
+++ setenv.sh
@@ -2,6 +2,10 @@ export DatasheetExtractor_source_path=${PWD}
 export DatasheetExtractor_examples_path=${DatasheetExtractor_source_path}/examples
 
 source /opt/python-virtual-env/py310/bin/activate
+
+export PYTHONPYCACHEPREFIX=${DatasheetExtractor_source_path}/.pycache
+export PYTHONDONTWRITEBYTECODE=${DatasheetExtractor_source_path}/.pycache
+
 append_to_python_path_if_not ${DatasheetExtractor_source_path}
 append_to_python_path_if_not ${DatasheetExtractor_source_path}/mamba-dist/
 append_to_ld_library_path_if_not ${DatasheetExtractor_source_path}/mamba-dist/
